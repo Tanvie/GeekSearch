d35397d82d92267a6be3fe75def7ec3251ea214d
Tanvi Wakade <tanvi.wakade19@vit.edu>
2021-03-23 00:41:49 +0530

    Added: basic architecture

--- app/src/main/java/com/example/geeksearch/ProfileActivity.kt
+++ app/src/main/java/com/example/geeksearch/ProfileActivity.kt
@@ -3,9 +3,9 @@ package com.example.geeksearch
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_profile)
     }
 }

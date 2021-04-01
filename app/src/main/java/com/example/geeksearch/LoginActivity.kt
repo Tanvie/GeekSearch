022d2474bebb608a846dd8b89a4133893e24bc44
Tanvi Wakade <tanvi.wakade19@vit.edu>
2021-04-01 19:36:52 +0530

    Added: user and org login activity.

--- app/src/main/java/com/example/geeksearch/LoginActivity.kt
+++ app/src/main/java/com/example/geeksearch/LoginActivity.kt
@@ -11,8 +11,17 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         val laBtSkip = findViewById<Button>(R.id.laBtSkip)
+        val laBtLoginUser = findViewById<Button>(R.id.laBtLoginUser)
+        val laBtLoginOrg = findViewById<Button>(R.id.laBtLoginOrg)
         laBtSkip.setOnClickListener {
             startActivity(Intent(this, HomeActivity::class.java))
+        }
+
+        laBtLoginUser.setOnClickListener {
+            startActivity(Intent(this, UserLoginActivity::class.java))
+        }
+        laBtLoginOrg.setOnClickListener {
+            startActivity(Intent(this, OrgLoginActivity::class.java))
         }
     }
 }

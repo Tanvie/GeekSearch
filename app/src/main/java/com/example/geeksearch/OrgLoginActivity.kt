6db6f1115c624796b82805c146974c60abae7e54
Manali Teke <teke.manali@gmail.com>
2021-04-02 22:30:07 +0530

    Updated: activity_org_login.xml

--- app/src/main/java/com/example/geeksearch/OrgLoginActivity.kt
+++ app/src/main/java/com/example/geeksearch/OrgLoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.geeksearch
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class OrgLoginActivity : AppCompatActivity() {
@@ -12,7 +13,9 @@ class OrgLoginActivity : AppCompatActivity() {
 
         val laBtSkip = findViewById<Button>(R.id.laBtSkip)
         val olBtRegister = findViewById<Button>(R.id.olBtRegister)
-
+        val olBtLogin = findViewById<Button>(R.id.olBtLogin)
+        val et_login_mail = findViewById<EditText>(R.id.et_login_mail)
+        val et_login_password = findViewById<EditText>(R.id.et_login_password)
 
         laBtSkip.setOnClickListener {
             startActivity(Intent(this, OrgHomeActivity::class.java))
@@ -22,5 +25,9 @@ class OrgLoginActivity : AppCompatActivity() {
             startActivity(Intent(this, OrgRegisterActivity::class.java))
         }
 
+        olBtLogin.setOnClickListener {
+            var orgMail = et_login_mail.text.toString()
+            var orgPassword = et_login_password.text.toString()
+        }
     }
 }

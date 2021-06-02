dad2f27a6ba78df0f57742f2f1ae13c6cec86aee
Tanvi Wakade <tanvi.wakade19@vit.edu>
2021-06-03 03:01:18 +0530

    Added: view inviter's profile

---
+++ app/src/main/java/com/example/geeksearch/ViewUserProfile.kt
@@ -0,0 +1,60 @@
+package com.example.geeksearch
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.geeksearch.databinding.ActivityViewUserProfileBinding
+import com.google.firebase.firestore.FirebaseFirestore
+
+
+class ViewUserProfile : AppCompatActivity() {
+    private lateinit var binding: ActivityViewUserProfileBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityViewUserProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val inviterEmail = intent.getStringExtra("inviterEmail")
+
+        val hackName = intent.getStringExtra("hack_name")
+        val db = FirebaseFirestore.getInstance()
+        val docRef = inviterEmail?.let { db.collection("Users").document(it) }
+        docRef?.get()?.addOnSuccessListener { document ->
+            if (document != null) {
+
+                val user: MutableMap<String, Any> = document.data as MutableMap<String, Any>
+                binding.tvInviterClg.text = user["userCollege"].toString()
+                binding.tvInviterDegree.text = user["userDegree"].toString()
+                binding.tvInviterName.text = user["userName"].toString()
+                binding.tvInviterEmail.text = user["userEmail"].toString()
+                binding.tvInviterPhoneNo.text = user["phoneNo"].toString()
+                binding.tvInviterLocation.text = user["userLocation"].toString()
+                binding.tvInviterCodechef.text = user["codeChefLink"].toString()
+                binding.tvInviterLinkedIn.text = user["linkedInLink"].toString()
+                binding.tvInviterHackerrank.text = user["hackerRankLink"].toString()
+                binding.tvInviterSkills.text = user["userSkills"].toString()
+                Toast.makeText(this, "Profile data fetched", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Error in Fetching user!", Toast.LENGTH_SHORT).show()
+            }
+        }?.addOnFailureListener {
+            Toast.makeText(this, "User Failed to fetch!", Toast.LENGTH_SHORT).show()
+        }
+
+
+        binding.btnInviterMailTo.setOnClickListener {
+            var send: String =
+                "mailto:$inviterEmail?subject=About Announcement made on GeekSearch about: $hackName"
+            val emailIntent = Intent(Intent.ACTION_SENDTO)
+            emailIntent.data = Uri.parse(send)
+            try {
+                startActivity(emailIntent)
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(this, "Error Displaying email", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/geeksearch/user/announcement/details/AnnouncementDetailsFragment.kt
+++ app/src/main/java/com/example/geeksearch/user/announcement/details/AnnouncementDetailsFragment.kt
@@ -1,10 +1,12 @@
 package com.example.geeksearch.user.announcement.details
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.geeksearch.ViewUserProfile
 import com.example.geeksearch.databinding.FragmentAnnouncementDetailsBinding
 import com.example.geeksearch.user.announcement.announcement.AnnouncementModel
 import de.cketti.mailto.EmailIntentBuilder
@@ -41,11 +43,21 @@ class AnnouncementDetailsFragment : Fragment() {
                 activity?.let { it2 ->
                     EmailIntentBuilder.from(it2)
                         .to(it1.userEmail)
-                        .subject("About Announcement on GeekSearch about: " + ann.hackName)
-                        .body("")
+                        .subject("About Announcement made on GeekSearch about: " + ann.hackName)
+                        .body("Hello")
                         .start()
                 }
             } == true
+        }
+
+        binding.btnViewUserProfile.setOnClickListener {
+            val intent = Intent(activity, ViewUserProfile::class.java)
+            if (ann != null) {
+                intent.putExtra("inviterEmail", ann.userEmail)
+                intent.putExtra("hack_name", ann.hackName)
+
+            }
+            startActivity(intent)
         }
         return view
     }

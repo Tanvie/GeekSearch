45263545d3566a120dfb5a46a4ed057801b0e693
Avinash Vijayvargiya <avinash14022002@gmail.com>
2021-06-06 00:43:28 +0530

    updated: UI with TM.

--- app/src/main/java/com/example/geeksearch/user/user/UserHomeActivity.kt
+++ app/src/main/java/com/example/geeksearch/user/user/UserHomeActivity.kt
@@ -11,17 +11,13 @@ import com.example.geeksearch.user.user.profile.UserProfileActivity
 class UserHomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityUserHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUserHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        val haIvProfile = findViewById<ImageView>(R.id.haIvProfile)
-//        val haBtChat = findViewById<Button>(R.id.haBtChat)
-//        val haBtCreateTeam = findViewById<Button>(R.id.haBtCreateTeam)
-//        val haBtJoinTeam = findViewById<Button>(R.id.haBtJoinTeam)
-
-        binding.imgBtnUserProfile.setOnClickListener {
+        binding.ivBtnUserProfile.setOnClickListener {
             startActivity(Intent(this, UserProfileActivity::class.java))
         }
 
